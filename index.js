55dd7ab4f1d631e808964f03a551b5e3ef1c9c39
Damien Bezborodov <dbezborodov@gmail.com>
2023-05-28 02:35:33 +0930

    Private bits.

--- index.js
+++ index.js
@@ -1,24 +1,30 @@
 class MutationInitialiser {
+  #selector;
+  #callback;
+  #options;
+  #enabled;
+  #observer;
+
   constructor(selector, callback, options = {}) {
-    this.selector = selector;
-    this.callback = callback;
-    this.options = Object.assign({}, options);
-    this.options.subtree ??= false;
-    this.options.watch ??= false;
-    this.options.many ??= false;
-    this.options.scope ??= 0;
+    this.#selector = selector;
+    this.#callback = callback;
+    this.#options = Object.assign({}, options);
+    this.#options.subtree ??= false;
+    this.#options.watch ??= false;
+    this.#options.many ??= false;
+    this.#options.scope ??= 0;
     if (!options.childList || !options.attributes || !options.characterData) {
-      this.options.childList = true;
+      this.#options.childList = true;
     }
-    this.observer = new MutationObserver((mutations) => {
+    this.#observer = new MutationObserver((mutations) => {
       for (const mutation of mutations) {
         if (mutation.type === 'childList') {
           for (const addedNode of mutation.addedNodes) {
             if (addedNode instanceof HTMLElement) {
-              if (addedNode.matches(this.selector)) {
-                this.call(addedNode);
+              if (addedNode.matches(this.#selector)) {
+                this.#call(addedNode);
 
-                if (!this.enabled) return;
+                if (!this.#enabled) return;
               }
             }
           }
@@ -26,50 +32,50 @@ class MutationInitialiser {
       }
     });
   }
-  call(element) {
-    if (!this.enabled) {
+  #call(element) {
+    if (!this.#enabled) {
       return;
     }
 
-    this.callback(element);
+    this.#callback(element);
 
-    if (!this.options.many) {
+    if (!this.#options.many) {
       // Disconnect after first call if many is not specified.
       this.disconnect();
     }
   }
-  firstPass(target) {
+  #firstPass(target) {
     // First pass. Find matches already on the page.
-    if (this.options.subtree) {
-      const matches = target.querySelectorAll(this.selector);
+    if (this.#options.subtree) {
+      const matches = target.querySelectorAll(this.#selector);
       for (const match of matches) {
-        this.call(match);
-        if (!this.enabled) return;
+        this.#call(match);
+        if (!this.#enabled) return;
       }
     } else {
       for (const child of target.children) {
-        if (child.matches(this.selector)) {
-          this.call(child);
-          if (!this.enabled) return;
+        if (child.matches(this.#selector)) {
+          this.#call(child);
+          if (!this.#enabled) return;
         }
       }
     }
   }
   observe(target) {
-    this.enabled = true;
+    this.#enabled = true;
 
-    this.firstPass(target);
+    this.#firstPass(target);
 
-    if (!this.enabled) return;
+    if (!this.#enabled) return;
 
-    if (!this.options.watch && document.readyState != 'loading') {
+    if (!this.#options.watch && document.readyState != 'loading') {
       // Document is already finished loading. Do not observe when not watching.
       return;
     }
 
-    this.observer.observe(target, this.options);
+    this.#observer.observe(target, this.#options);
 
-    if (!this.options.watch) {
+    if (!this.#options.watch) {
       // If not watching, disconnect after DOM is loaded.
       window.addEventListener('DOMContentLoaded', () => {
         this.disconnect();
@@ -77,11 +83,11 @@ class MutationInitialiser {
     }
   }
   disconnect() {
-    this.enabled = false;
-    this.observer.disconnect();
+    this.#enabled = false;
+    this.#observer.disconnect();
   }
   takeRecords() {
-    return this.observer.takeRecords();
+    return this.#observer.takeRecords();
   }
 }
 
